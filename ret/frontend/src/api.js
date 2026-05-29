7dfd441260f608bd5616f07e93d4e4ddf93308dd
Sneha Gade <sgade5591@gmail.com>
2026-05-29 15:42:01 +0530

    Debug API URL

--- ret/frontend/src/api.js
+++ ret/frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 // set your backend URL here
+console.log("API URL =", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
