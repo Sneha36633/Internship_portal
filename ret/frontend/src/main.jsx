bbf3aa916f6349b810ad64ba9e46a97a307e3c5d
Sneha Gade <sgade5591@gmail.com>
2026-05-29 15:52:43 +0530

    Test new build

--- ret/frontend/src/main.jsx
+++ ret/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; // Your main css file
 import { BrowserRouter } from 'react-router-dom';
+console.log("NEW BUILD LOADED");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
